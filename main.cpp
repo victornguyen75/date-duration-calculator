f04e6778582ef7fc27b5d288f051d079c2bcf400
Victor Nguyen <vdn140030@gmail.com>
2020-11-13 19:08:45 -0600

    feat(main.cpp): add date validation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 struct date
@@ -6,11 +7,37 @@ struct date
   int year;
   int month;
   int day;
+  string error;
+  string validate()
+  {
+    if (day < 1 || day > 31)
+    {
+      return "this day is invalid.";
+    }
+    else if (month < 1 || month > 12)
+    {
+      return "this month is invalid.";
+    }
+    else if (year < 0)
+    {
+      return "this year is invalid.";
+    }
 
-  // Date validation: years, months, days (take into account leap year)
-  //   a. No alphabets or special characters; only positive integers
-  //   b. Month validation: between 1-13
-  //   c. Day validation: between 1-31 (depending on month and leap year)
+    if ((month == 4 || month == 6 || month == 9 || month == 11) && day == 31)
+    {
+      return "day 31 does not exist for this month.";
+    }
+    else if ((month == 2) && (year % 4 == 0) && day > 29)
+    {
+      return "this day is invalid for Febuary.";
+    }
+    else if ((month == 2) && (year % 4 != 0) && day > 28)
+    {
+      return "this day is invalid for Febuary.";
+    }
+
+    return "None.";
+  }
 };
 
 int rataDie(int month, int day, int year);
@@ -22,14 +49,25 @@ int main()
   int difference = 0;
 
   cout << "Please enter two dates: ";
-
   scanf("%d/%d/%d %d/%d/%d", &date1.month, &date1.day, &date1.year, &date2.month, &date2.day, &date2.year);
-
   printf("\nDate 1: %d/%d/%d\nDate 2: %d/%d/%d\n", date1.month, date1.day, date1.year, date2.month, date2.day, date2.year);
 
-  difference = rataDie(date2.month, date2.day, date2.year) - rataDie(date1.month, date1.day, date1.year);
+  date1.error = date1.validate();
+  date2.error = date2.validate();
 
-  cout << "The difference between the two dates is " << difference << " days." << endl;
+  if (date1.error != "None.")
+  {
+    cout << "\nDate 1 is invalid because " << date1.error << endl;
+    return 0;
+  }
+  else if (date2.error != "None.")
+  {
+    cout << "\nDate 2 is invalid because " << date2.error << endl;
+    return 0;
+  }
+
+  difference = rataDie(date2.month, date2.day, date2.year) - rataDie(date1.month, date1.day, date1.year);
+  cout << "The difference between the two dates is " << difference << " day(s)." << endl;
   return 0;
 }
 
