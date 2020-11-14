780472f9f1abb4554b0e3374a291da34c2aaf492
Victor Nguyen <vdn140030@gmail.com>
2020-11-13 20:55:18 -0600

    refactor(main.cpp): create a validateInput() function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "Date.h"
 
 int rataDie(int month, int day, int year);
+void validateInput(Date date1, Date date2);
 
 int main()
 {
@@ -13,19 +14,7 @@ int main()
   scanf("%d/%d/%d %d/%d/%d", &date1.month, &date1.day, &date1.year, &date2.month, &date2.day, &date2.year);
   printf("\nDate 1: %d/%d/%d\nDate 2: %d/%d/%d\n", date1.month, date1.day, date1.year, date2.month, date2.day, date2.year);
 
-  date1.validate();
-  date2.validate();
-
-  if (date1.getError() != "None.")
-  {
-    std::cout << "\nDate 1 is invalid because " << date1.getError() << std::endl;
-    return 0;
-  }
-  else if (date2.getError() != "None.")
-  {
-    std::cout << "\nDate 2 is invalid because " << date2.getError() << std::endl;
-    return 0;
-  }
+  validateInput(date1, date2);
 
   difference = rataDie(date2.month, date2.day, date2.year) - rataDie(date1.month, date1.day, date1.year);
   std::cout << "The difference between the two dates is " << difference << " day(s)." << std::endl;
@@ -41,4 +30,21 @@ int rataDie(int month, int day, int year)
   }
 
   return 365 * year + year / 4 - year / 100 + year / 400 + (153 * month - 457) / 5 + day - 306;
+}
+
+void validateInput(Date date1, Date date2)
+{
+  date1.validate();
+  date2.validate();
+
+  if (date1.getError() != "None.")
+  {
+    std::cout << "\nDate 1 is invalid because " << date1.getError() << std::endl;
+    exit(1);
+  }
+  else if (date2.getError() != "None.")
+  {
+    std::cout << "\nDate 2 is invalid because " << date2.getError() << std::endl;
+    exit(1);
+  }
 }
